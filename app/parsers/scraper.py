ddd53bc5e0b103dd3d1a1c38f8302cbf65326bac
bissaliev <bissaliev21@gmail.com>
2025-02-23 10:52:37 +0400

    scraper.py: Добавил докстринги и аннотации

--- app/parsers/scraper.py
+++ app/parsers/scraper.py
@@ -1,18 +1,46 @@
 import io
 
 import requests
+from exceptions import RequestProcessingError
+from logging_config import logger
+from requests.exceptions import ConnectionError, RequestException, Timeout
+
+
+def get_request(url: str) -> requests.Response:
+    """Выполняет GET-запрос по указанному URL и возвращает объект Response"""
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+        return response
+    except (ConnectionError, Timeout) as e:
+        raise RequestProcessingError(
+            f"Ошибка при подключении или тайм-аут для страницы {url}: {e}"
+        ) from e
+    except RequestException as e:
+        raise RequestProcessingError(
+            f"Ошибка запроса при получении страницы {url}: {e}"
+        ) from e
+    except Exception as e:
+        raise RequestProcessingError(
+            f"Неизвестная ошибка при получении страницы {url}: {e}"
+        ) from e
 
 
 def get_page(url: str) -> str:
-    """Получает HTML-код страницы и текст страницы."""
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.text
+    """Получает HTML-код страницы по указанному URL"""
+    try:
+        response = get_request(url)
+        return response.text
+    except RequestProcessingError as e:
+        logger.error(str(e))
+        raise RequestProcessingError(e) from e
 
 
 def download_file_as_bytes(url: str) -> io.BytesIO:
-    """Скачивает файл по ссылке и возвращает его в виде потока байтов."""
-    response = requests.get(url)
-    response.raise_for_status()
-
-    return io.BytesIO(response.content)
+    """Скачивает файл по ссылке и возвращает его в виде потока байтов"""
+    try:
+        response = get_request(url)
+        return io.BytesIO(response.content)
+    except RequestProcessingError as e:
+        logger.error(str(e))
+        raise RequestProcessingError(e) from e
