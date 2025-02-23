dfcc7533aa5bd126cdab6d7c723db644891e8494
bissaliev <bissaliev21@gmail.com>
2025-02-23 10:37:09 +0400

    logging_config.py: Добавил название модуля в лог

--- app/logging_config.py
+++ app/logging_config.py
@@ -10,7 +10,7 @@ LOG_FILE = LOG_DIR / "app.log"
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    format="%(asctime)s [%(levelname)s] %(module)s: %(message)s",
     handlers=[
         logging.FileHandler(LOG_FILE, encoding="utf-8"),
         logging.StreamHandler(sys.stdout),
